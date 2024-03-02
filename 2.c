8df62988fe69c3c17b55f998a5838f0e1fb21bd7
2semClab <kushsingh2004+1@gmail.com>
2024-03-02 17:04:13 +0530

    Correcting 2.c

--- 2.c
+++ 2.c
@@ -2,22 +2,17 @@
 int main(){
 int n;
 printf("Enter a number : ");
+int flag =0;
 scanf("%d",&n);
-if (n==2)
-	printf("%d is a prime number\n",n);
-else{
 for(int i=2;i<n;i++){
 	if(n%i == 0){
 		printf("%d is not a prime number\n",n);
+		flag=1;
 		break;
 	}
-	else{
+}
+if (flag ==0){
 	printf("%d is a prime number\n",n);
-	break;
-	
-	}
 }
-}
-
 return 0;
 }
